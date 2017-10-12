9801b2839ea4ebfb3026418f1d4abead0301a1c6
Yasir Ali <yasir.ali@asynchrony.com>
2017-10-12 15:52:28 -0500

    Initial project setup

---
+++ LazyThermometer/LazyThermometerTests/Lazy_ThermometerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lazy_ThermometerTests.swift
+//  Lazy ThermometerTests
+//
+//  Created by Yasir Ali on 12/14/16.
+//  Copyright © 2016 yAli Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import Lazy_Thermometer
+
+class Lazy_ThermometerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
